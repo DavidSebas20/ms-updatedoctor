ad88e945bfa900faa40179742aa246b3cfcfa4a3
DavidSebas20 <davidsebastian20@outlook.com>
2025-02-08 16:38:25 -0500

    :tada: initialize updatedoctor project with Spring Boot and Maven setup

---
+++ src/main/java/com/example/updatedoctor/UpdatedoctorApplication.java
@@ -0,0 +1,13 @@
+package com.example.updatedoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdatedoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdatedoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/updatedoctor/UpdatedoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.updatedoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdatedoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
